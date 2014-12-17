b79146e45349f2d1468d4918df47db5abdad7529
Quepas <oneinchman.dev@gmail.com>
2014-12-17 11:04:52 +0100

    Adding data set to all data sets comparision.

--- DataSetSimiliarityMetrics/similiarity_metrics.R
+++ DataSetSimiliarityMetrics/similiarity_metrics.R
@@ -4,20 +4,70 @@ library("proxy")
 iris <- read.arff("data/iris.arff")
 iris2d <- read.arff("data/iris.2D.arff")
 
+find_similiar_dataset <- function(base_ds, directory) {
+  file_names <- list.files(directory, pattern="\\.arff$")
+
+  file_names <- file_names[file_names != base_ds]
+  base <- read.arff(paste(directory, base_ds, sep="\\"))
+  max_simil <- 0
+  winner <- "NA"
+  for (name in file_names) {
+    print(name)
+    ds <- read.arff(paste(directory, name, sep="\\"))
+    simil_metrics <- compare_datasets(base, ds)
+    if (simil_metrics > max_simil) {
+      max_simil <- simil_metrics
+      winner <- name
+    }
+    cat(sprintf("Comparing with %s: %f\n", toString(name), simil_metrics))
+  }
+  cat("\nWinner is", winner, "with", toString(max_simil))
+}
+
 compare_datasets <- function(ds1, ds2) {
   ds1_numeric <- filter_for_numeric(ds1)
   ds2_numeric <- filter_for_numeric(ds2)
 
-  cat(sprintf("ds1 ncol: %i\n", ncol(ds1)))
-  cat(sprintf("ds1 numeric ncol: %i\n", ncol(ds1_numeric)))
-  cat(sprintf("ds2 ncol: %i\n", ncol(ds2)))
-  cat(sprintf("ds2 numeric ncol: %i\n", ncol(ds2_numeric)))
+  ds1_ncol_n <- ncol(ds1_numeric)
+  ds2_ncol_n <- ncol(ds2_numeric)
 
-  for (i in 1:ncol(ds1_numeric)) {
-    for (j in 1:ncol(ds2_numeric)) {
-      cat(sprintf("%i x %i = %f\n", i, j, calculate_simil(ds1_numeric[,i], ds2_numeric[,j])))
-    }
+  if (ds1_ncol_n == 0 || ds2_ncol_n == 0) {
+    return(-1)
   }
+
+#   cat(sprintf("ds1 ncol: %i\n", ncol(ds1)))
+#   cat(sprintf("ds1 numeric ncol: %i\n", ds1_ncol_n))
+#   cat(sprintf("ds2 ncol: %i\n", ncol(ds2)))
+#   cat(sprintf("ds2 numeric ncol: %i\n", ds2_ncol_n))
+
+  if (ds2_ncol_n > ds1_ncol_n) {
+    temp <- ds1_numeric
+    ds1_numeric <- ds2_numeric
+    ds2_numeric <- temp
+  
+    ds1_ncol_n <- ncol(ds1_numeric)
+    ds2_ncol_n <- ncol(ds2_numeric)
+  }
+
+  column_matching <- 0
+#   cat("----------------- Column matching -------------------\n")
+  for (i in 1:ds1_ncol_n) {
+    max_simil <- 0
+    counter <- 0
+    for (j in 1:ds2_ncol_n) {
+      col_simil <- calculate_simil(ds1_numeric[,i], ds2_numeric[,j])
+#       cat(sprintf("%i x %i = %f\n", i, j, col_simil))
+
+      if (col_simil > max_simil) {
+        max_simil <- col_simil
+        counter <- j
+      }
+    }
+#     cat(sprintf("Matched columnds: %i x %i\n", i, counter))
+    column_matching <- column_matching + max_simil
+  }
+#   cat("-----------------------------------------------------\n")
+  column_matching/ds1_ncol_n
 }
 
 filter_for_numeric <- function(ds) {
