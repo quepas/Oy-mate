47f93ae0763439722dbf9ae90c3995397d259f77
Quepas <oneinchman.dev@gmail.com>
2015-01-22 18:28:34 +0100

    Adding two basic pojo-like structures.

---
+++ OpenMLApps/src/pl/edu/agh/oymate/pojo/ImplementationSimple.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.oymate.pojo;
+
+/**
+ * Created by Quepas on 2015-01-22.
+ */
+public class ImplementationSimple {
+    public int id;
+    public String name;
+}
---
+++ OpenMLApps/src/pl/edu/agh/oymate/pojo/RunEvaluation.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.oymate.pojo;
+
+/**
+ * Created by Quepas on 2015-01-22.
+ */
+public class RunEvaluation {
+    public String function;
+    public double value;
+    public double stddev;
+    // String for now
+    public String array_data;
+}
