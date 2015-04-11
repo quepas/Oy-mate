aa7ff1400005c461ae46cef4785f63e78f91fdf4
Quepas <oneinchman.dev@gmail.com>
2015-04-11 19:46:01 +0200

    Adding function for testing WEKA model.

--- ml-auto-configuration-rules/weka-utils.R
+++ ml-auto-configuration-rules/weka-utils.R
@@ -1,4 +1,5 @@
 library(tools)
+library(RWeka)
 
 # Generate java-based classificator model from given data set in *.arff file
 GenerateWEKAModel <- function(dataSetFile) {
@@ -7,5 +8,13 @@ GenerateWEKAModel <- function(dataSetFile) {
   runCommand <- paste("java", "-classpath", wekaPathInQuotes,
                       "weka.classifiers.trees.J48", "-t", paste(generatedDatasetDir, dataSetFile, sep=""),
                       "-d", modelFile)
+  system(runCommand)
+}
+
+TestWEKAModel <- function(modelName, testSet) {
+  wekaPathInQuotes <- paste("\"", wekaJarPath, "\"", sep="")
+  runCommand <- paste("java", "-classpath", wekaPathInQuotes,
+                      "weka.classifiers.trees.J48", "-l", paste(generatedModelDir, modelName, ".model", sep=""),
+                      "-T", paste(generatedTestSetDir, testSet, ".arff", sep=""))
   system(runCommand)
 }
