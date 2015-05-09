616c9bc5bdf93ba58dc94bb28666f99b0649cea4
Quepas <oneinchman.dev@gmail.com>
2015-05-09 23:44:01 +0200

    Adding saving XARFF files (#2).

--- ml-auto-configuration-rules/file-utils.R
+++ ml-auto-configuration-rules/file-utils.R
@@ -9,3 +9,25 @@ SaveARFF <- function(dataFrame, fileName, directory = "") {
 LoadARFF <- function(fileName, directory = "") {
   read.arff(paste(directory, fileName, ".arff", sep=""))
 }
+
+# Saving data frame as XARRF ranking file
+SaveXARFF <- function(dataFrame, fileName, directory = "", ranks) {
+  # Save as normal ARFF
+  SaveARFF(dataFrame, fileName, directory)
+  # replace combined ranks with single ranks
+  # - load file
+  file <- scan(sep = "\n", paste(directory, fileName, ".arff", sep=""), character(0))
+  print(file)
+  # - find '@attribute ranks ...' occurence
+  counter <- 1
+  for (line in file) {
+    if (grepl("@attribute rank", line)) {
+      replacement <- paste("@attribute ranks RANKING {", paste(ranks, collapse = ","), "}")
+      file[counter] <- replacement
+      break;
+    }
+    counter <- counter + 1
+  }
+  # - rewrite file
+  cat(file,file=paste(directory, fileName, ".arff", sep=""),sep="\n")
+}
