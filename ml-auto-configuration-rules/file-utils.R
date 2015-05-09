ecb18de48a9b7c9040e588f9e26ac06c52aac63a
Quepas <oneinchman.dev@gmail.com>
2015-05-10 00:15:24 +0200

    Changing XARFF file extension to proper one (#2).

--- ml-auto-configuration-rules/file-utils.R
+++ ml-auto-configuration-rules/file-utils.R
@@ -1,23 +1,23 @@
 library(foreign)
 
 # Saving data frame as ARFF file, fileName without extension
-SaveARFF <- function(dataFrame, fileName, directory = "") {
-  write.arff(dataFrame, paste(directory, fileName, ".arff", sep=""))
+SaveARFF <- function(dataFrame, fileName, directory = "", extension = "arff") {
+  write.arff(dataFrame, paste(directory, fileName, ".", extension, sep=""))
 }
 
 # Loading ARFF file as data frame, fileName without extension
-LoadARFF <- function(fileName, directory = "") {
-  read.arff(paste(directory, fileName, ".arff", sep=""))
+LoadARFF <- function(fileName, directory = "", extension = "arff") {
+  read.arff(paste(directory, fileName, ".", extension, sep=""))
 }
 
 # Saving data frame as XARRF ranking file
 SaveXARFF <- function(dataFrame, fileName, directory = "", ranks) {
   # Save as normal ARFF
-  SaveARFF(dataFrame, fileName, directory)
+  SaveARFF(dataFrame, fileName, directory, extension = "xarff")
   # replace combined ranks with single ranks
   # - load file
   file <- scan(sep = "\n", paste(directory, fileName, ".arff", sep=""), character(0))
-  print(file)
+  #print(file)
   # - find '@attribute ranks ...' occurence
   counter <- 1
   for (line in file) {
