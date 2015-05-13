128e44c70d12a8463f7b583565dcb4b3897f7fa9
Quepas <oneinchman.dev@gmail.com>
2015-05-13 22:01:03 +0200

    Adding WEKA-LR path.

--- ml-auto-configuration-rules/setup-environment.R
+++ ml-auto-configuration-rules/setup-environment.R
@@ -1,5 +1,6 @@
 SetupGlobalEnv <- function() {
   wekaJarPath <<- "D:/Programy2/Weka-3-6/weka.jar"
+  wekaLRRun <<- "D:/Studia AGH/Praca mgr/tools/WEKA-LR/runWekaSimple.bat"
   generatedModelDir <<- "./generated-model/"
   generatedDatasetDir <<- "./generated-dataset/"
   generatedTestSetDir <<- "./generated-test-set/"
