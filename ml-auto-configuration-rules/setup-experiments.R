9aefb8ed0a1e6ec160e98bed66139367c6e02e37
Quepas <oneinchman.dev@gmail.com>
2015-06-05 19:48:57 +0200

    Adding additional ML algorithm to criterias, adding function returning top N algorithms from OpenML (by usage).

--- ml-auto-configuration-rules/setup-experiments.R
+++ ml-auto-configuration-rules/setup-experiments.R
@@ -1,23 +1,54 @@
 CreateAlgorithmsFrame <- function() {
-  name <- c(#"weka.OneR",
-            #"weka.ZeroR",
+  name <- c("weka.OneR",
+            "weka.ZeroR",
             "weka.J48",
             "weka.NaiveBayes",
             "weka.Logistic",
             "weka.RandomTree",
             "weka.JRip",
             "weka.A1DE",
-            "weka.Bagging_REPTree")
-  openmlName <- c(#"^weka.OneR\\(.*\\)$",
-                  #"^weka.ZeroR\\(.*\\)$",
+            "weka.Bagging_REPTree",
+            "weka.FilteredClassifier_Discretize_J48",
+            "weka.BayesNet_K2",
+            "weka.LibSVM",
+            "weka.ADTree",
+            "weka.AdaBoostM1_DecisionStump",
+            "weka.NBTree")
+  openmlName <- c("^weka.OneR\\(.*\\)$",
+                  "^weka.ZeroR\\(.*\\)$",
                   "^weka.J48\\(.*\\)$",
                   "^weka.NaiveBayes\\(.*\\)$",
                   "^weka.Logistic\\(.*\\)$",
                   "^weka.RandomTree\\(.*\\)$",
                   "^weka.JRip\\(.*\\)$",
                   "^weka.A1DE\\(.*\\)$",
-                  "^weka.Bagging_REPTree\\(.*\\)")
+                  "^weka.Bagging_REPTree\\(.*\\)",
+                  "^weka.FilteredClassifier_Discretize_J48\\(.*\\)",
+                  "^weka.BayesNet_K2\\(.*\\)",
+                  "^weka.LibSVM\\(.*\\)",
+                  "^weka.ADTree\\(.*\\)",
+                  "^weka.AdaBoostM1_DecisionStump\\(.*\\)")
   data.frame(name, openmlName)
+}
+
+GetTopNAlgorithms <- function(N) {
+  impl <- globalResults$implementation
+  # contingency table
+  impl <- sort(table(impl), decreasing =  T)
+  # get only WEKA's algorithms
+  impl <- impl[grepl("^weka.*$", names(impl))]
+  # get only top N algorithms
+  impl <- names(head(impl, N))
+  name <- c()
+  openmlName <- c()
+  for (impl_name in impl) {
+    base_name <- sub("\\(.*\\)", "", impl_name)
+    name <- c(name, base_name)
+    base_name_regexp <- paste(base_name, "\\(.*\\)", sep = "")
+    openmlName <- c(openmlName, base_name_regexp)
+  }
+  df <- data.frame(name, openmlName)
+  df[unique(df$name), ]
 }
 
 CreateAlgorithmsFrameAll <- function() {
