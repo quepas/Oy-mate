12e9ddce7b84f924bfbd2f8f53bd1a7a951c37cc
Quepas <oneinchman.dev@gmail.com>
2015-06-23 23:31:05 +0200

    Minor changes in meta-knowledge experiment: classic build.

--- meta-learning-system/experiment-meta-knowledge.R
+++ meta-learning-system/experiment-meta-knowledge.R
@@ -2,10 +2,10 @@ source('system-meta-knowledge.R')
 
 ClassicBuild <- function() {
   meta.attributes <- GenerateMetaAttributes(meta.attr.coverage = 0.3, meta.attr = envMetaAttributes, meta.attr.fill = TRUE)
-  algorithm.list <- ListNTopUsedAlgorithms(2, task.evaluations = envTaskEvaluations)$algorithm
+  algorithm.list <- ListNTopUsedAlgorithms(10, task.evaluations = envTaskEvaluations)$algorithm
   
   FilterForAlgorithms(algorithm.list, task.evaluations = envTaskEvaluations) %>%
-    filter(area.under.roc.curve > 0.99) -> task.evaluations
+    filter(area.under.roc.curve > 0.98) -> task.evaluations
   task.to.dataset <- RetriveDatasetId(task.evaluations$task.id)
   
   meta.knowledge <- data.frame()
@@ -16,11 +16,12 @@ ClassicBuild <- function() {
     dataset.id <- unique(RetriveDatasetId(task.id.list)$dataset.id)
     meta.knowledge <- rbind(meta.knowledge, BuildMetaKnowledgeChunk(dataset.id, algorithm, meta.attributes))
   }
-  CheckBuild(meta.knowledge)
+  BuildMetaRules(hmr.file = "ClassicBuild.hmr", meta.knowledge = meta.knowledge)
 }
 
 CheckBuild <- function(build) {
   classifier <- J48(class ~ ., data = build, control = Weka_control(U = TRUE))
   print(classifier)
   print(evaluate_Weka_classifier(classifier, numFolds = 10))
+  classifier
 }
