67a67aa2ad856265208bd8b356f081c11ff5a11b
Quepas <oneinchman.dev@gmail.com>
2015-06-27 00:37:29 +0200

    Output classification statistics to file.

--- meta-learning-system/experiment-framework.R
+++ meta-learning-system/experiment-framework.R
@@ -3,17 +3,19 @@ library(RWeka)
 source('system-hmr-generator.R')
 source('system-weka-extractor.R')
 
-BuildMetaRules <- function(hmr.file, meta.knowledge, hmr.directory = "meta-rules/") {
-  tree <- BuildRuleBase(meta.knowledge)
+BuildMetaRules <- function(hmr.file, meta.knowledge, hmr.directory = "meta-rules/", statistics) {
+  tree <- BuildRuleBase(meta.knowledge, statistics)
   rules <- ParseRWekaTree(tree)
   hmr <- GenerateHMR(rules, meta.knowledge)
   write(hmr, paste(hmr.directory, hmr.file, sep = ""))
 }
 
-BuildRuleBase <- function(build) {
+BuildRuleBase <- function(build, statistics) {
   classifier <- J48(class ~ ., data = build, control = Weka_control(U = TRUE))
+  sink(file = statistics, append = TRUE)
   print(classifier)
-  print(evaluate_Weka_classifier(classifier, numFolds = 10))
+  print(evaluate_Weka_classifier(classifier, numFolds = 10, complexity = F))
+  sink()
   classifier
 }
 
